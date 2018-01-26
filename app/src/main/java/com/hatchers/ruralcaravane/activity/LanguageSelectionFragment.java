c8b1d29ac027baa615b73b83ffe1becb2746d944
origin <nikam.gopika94@gmail.com>
2018-01-26 22:42:08 +0530

    language selection bygopika

--- app/src/main/java/com/hatchers/ruralcaravane/activity/LanguageSelectionFragment.java
+++ app/src/main/java/com/hatchers/ruralcaravane/activity/LanguageSelectionFragment.java
@@ -16,6 +16,7 @@ public class LanguageSelectionFragment extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
